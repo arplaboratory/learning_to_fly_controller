93075cc6674f527b09394a93438c5d2846c43dbd
jonas-eschmann <jonas.eschmann@me.com>
2023-06-05 19:12:14 +0400

    it is flying well (4 minutes and counting)

--- backprop_tools_adapter.cpp
+++ backprop_tools_adapter.cpp
@@ -13,6 +13,8 @@
 
 // #define BACKPROP_TOOLS_CONTROL_STATE_QUATERNION
 #define BACKPROP_TOOLS_CONTROL_STATE_ROTATION_MATRIX
+// #define BACKPROP_TOOLS_DISABLE_TEST
+#define BACKPROP_TOOLS_ACTION_HISTORY
 
 
 // Definitions
@@ -24,12 +26,16 @@ DEVICE device;
 using ACTOR_TYPE = decltype(bpt::checkpoint::actor::mlp);
 using TI = typename ACTOR_TYPE::SPEC::TI;
 using T = typename ACTOR_TYPE::SPEC::T;
+constexpr TI CONTROL_FREQUENCY_MULTIPLE = 1;
+static TI controller_tick = 0;
 
 // State
 static ACTOR_TYPE::template Buffers<1, bpt::MatrixStaticTag> buffers;
 static bpt::MatrixStatic<bpt::matrix::Specification<T, TI, 1, ACTOR_TYPE::SPEC::INPUT_DIM>> input;
 static bpt::MatrixStatic<bpt::matrix::Specification<T, TI, 1, ACTOR_TYPE::SPEC::OUTPUT_DIM>> output;
+#ifdef BACKPROP_TOOLS_ACTION_HISTORY
 static T action_history[bpt::checkpoint::environment::ACTION_HISTORY_LENGTH][ACTOR_TYPE::SPEC::OUTPUT_DIM];
+#endif
 
 
 // Helper functions (without side-effects)
@@ -66,18 +72,30 @@ void backprop_tools_init(){
     bpt::malloc(device, buffers);
     bpt::malloc(device, input);
     bpt::malloc(device, output);
+#ifdef BACKPROP_TOOLS_ACTION_HISTORY
     for(TI step_i = 0; step_i < bpt::checkpoint::environment::ACTION_HISTORY_LENGTH; step_i++){
         for(TI action_i = 0; action_i < ACTOR_TYPE::SPEC::OUTPUT_DIM; action_i++){
             action_history[step_i][action_i] = 0;
         }
     }
+#endif
+    controller_tick = 0;
 }
 
 float backprop_tools_test(float* output_mem){
-#ifndef BACKPROP_TOOLS_CONTROL_STATE_QUATERNION
-    auto state = bpt::view(device, bpt::checkpoint::state::container, bpt::matrix::ViewSpec<1, 13>{}, 0, 0);
-    // observe_rotation_matrix(state, input);
-    // bpt::evaluate(device, bpt::checkpoint::actor::mlp, input, output, buffers);
+#ifndef BACKPROP_TOOLS_DISABLE_TEST
+// #ifndef BACKPROP_TOOLS_CONTROL_STATE_QUATERNION
+//     auto state = bpt::view(device, bpt::checkpoint::state::container, bpt::matrix::ViewSpec<1, 13>{}, 0, 0);
+//     // observe_rotation_matrix(state, input);
+//     // bpt::evaluate(device, bpt::checkpoint::actor::mlp, input, output, buffers);
+//     bpt::evaluate(device, bpt::checkpoint::actor::mlp, bpt::checkpoint::observation::container, output, buffers);
+//     float acc = 0;
+//     for(int i = 0; i < ACTOR_TYPE::SPEC::OUTPUT_DIM; i++){
+//         acc += std::abs(bpt::get(output, 0, i) - bpt::get(bpt::checkpoint::action::container, 0, i));
+//         output_mem[i] = bpt::get(bpt::checkpoint::action::container, 0, i);
+//     }
+//     return acc;
+// #elif defined(BACKPROP_TOOLS_CONTROL_STATE_ROTATION_MATRIX)
     bpt::evaluate(device, bpt::checkpoint::actor::mlp, bpt::checkpoint::observation::container, output, buffers);
     float acc = 0;
     for(int i = 0; i < ACTOR_TYPE::SPEC::OUTPUT_DIM; i++){
@@ -85,14 +103,9 @@ float backprop_tools_test(float* output_mem){
         output_mem[i] = bpt::get(bpt::checkpoint::action::container, 0, i);
     }
     return acc;
-#elif defined(BACKPROP_TOOLS_CONTROL_STATE_ROTATION_MATRIX)
-    bpt::evaluate(device, bpt::checkpoint::actor::mlp, bpt::checkpoint::state::container, output, buffers);
-    float acc = 0;
-    for(int i = 0; i < ACTOR_TYPE::SPEC::OUTPUT_DIM; i++){
-        acc += std::abs(bpt::get(output, 0, i) - bpt::get(bpt::checkpoint::action::container, 0, i));
-        output_mem[i] = bpt::get(bpt::checkpoint::action::container, 0, i);
-    }
-    return acc;
+// #endif
+#else
+    return 0;
 #endif
 }
 
@@ -102,27 +115,38 @@ void backprop_tools_control(float* state, float* actions){
     bpt::MatrixDynamic<bpt::matrix::Specification<T, TI, 1, 13, bpt::matrix::layouts::RowMajorAlignment<TI, 1>>> state_matrix = {(T*)state}; 
     bpt::MatrixDynamic<bpt::matrix::Specification<T, TI, 1, ACTOR_TYPE::SPEC::OUTPUT_DIM, bpt::matrix::layouts::RowMajorAlignment<TI, 1>>> output = {(T*)actions};
     bpt::evaluate(device, bpt::checkpoint::actor::mlp, state_matrix, output, buffers);
+    controller_tick++;
 }
 #elif defined(BACKPROP_TOOLS_CONTROL_STATE_ROTATION_MATRIX)
 void backprop_tools_control(float* state, float* actions){
     bpt::MatrixDynamic<bpt::matrix::Specification<T, TI, 1, 13, bpt::matrix::layouts::RowMajorAlignment<TI, 1>>> state_matrix = {(T*)state}; 
     auto state_rotation_matrix_input = bpt::view(device, input, bpt::matrix::ViewSpec<1, 18>{}, 0, 0);
     observe_rotation_matrix(state_matrix, state_rotation_matrix_input);
+#ifdef BACKPROP_TOOLS_ACTION_HISTORY
     auto action_history_observation = bpt::view(device, input, bpt::matrix::ViewSpec<1, bpt::checkpoint::environment::ACTION_HISTORY_LENGTH * ACTOR_TYPE::SPEC::OUTPUT_DIM>{}, 0, 18);
     for(TI step_i = 0; step_i < bpt::checkpoint::environment::ACTION_HISTORY_LENGTH; step_i++){
         for(TI action_i = 0; action_i < ACTOR_TYPE::SPEC::OUTPUT_DIM; action_i++){
             bpt::set(action_history_observation, 0, step_i * ACTOR_TYPE::SPEC::OUTPUT_DIM + action_i, action_history[step_i][action_i]);
         }
     }
+#endif
     bpt::MatrixDynamic<bpt::matrix::Specification<T, TI, 1, ACTOR_TYPE::SPEC::OUTPUT_DIM, bpt::matrix::layouts::RowMajorAlignment<TI, 1>>> output = {(T*)actions};
     bpt::evaluate(device, bpt::checkpoint::actor::mlp, input, output, buffers);
-    for(TI step_i = 0; step_i < bpt::checkpoint::environment::ACTION_HISTORY_LENGTH - 1; step_i++){
-        for(TI action_i = 0; action_i < ACTOR_TYPE::SPEC::OUTPUT_DIM; action_i++){
-            action_history[step_i][action_i] = action_history[step_i + 1][action_i];
+#ifdef BACKPROP_TOOLS_ACTION_HISTORY
+    if(controller_tick % CONTROL_FREQUENCY_MULTIPLE == 0){
+        for(TI step_i = 0; step_i < bpt::checkpoint::environment::ACTION_HISTORY_LENGTH - 1; step_i++){
+            for(TI action_i = 0; action_i < ACTOR_TYPE::SPEC::OUTPUT_DIM; action_i++){
+                action_history[step_i][action_i] = action_history[step_i + 1][action_i];
+            }
         }
     }
     for(TI action_i = 0; action_i < ACTOR_TYPE::SPEC::OUTPUT_DIM; action_i++){
-        action_history[bpt::checkpoint::environment::ACTION_HISTORY_LENGTH - 1][action_i] = bpt::get(output, 0, action_i);
+        if(controller_tick % CONTROL_FREQUENCY_MULTIPLE == 0){
+            action_history[bpt::checkpoint::environment::ACTION_HISTORY_LENGTH - 1][action_i] = 0;
+        }
+        action_history[bpt::checkpoint::environment::ACTION_HISTORY_LENGTH - 1][action_i] += bpt::get(output, 0, action_i) / ((T)CONTROL_FREQUENCY_MULTIPLE);
     }
+#endif
+    controller_tick++;
 }
 #endif
