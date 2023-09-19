de12ce2d7c061f7563fd99676c1727cd6a0e582b
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-09-19 18:55:05 -0400

    independent of action history checkpoint

--- backprop_tools_adapter.h
+++ backprop_tools_adapter.h
@@ -10,4 +10,9 @@ float backprop_tools_test(float*);
 extern "C"
 #endif
 void backprop_tools_control(float* state, float* actions);
+#ifdef __cplusplus
+extern "C"
+#endif
+char* backprop_tools_get_checkpoint_name();
+
 
