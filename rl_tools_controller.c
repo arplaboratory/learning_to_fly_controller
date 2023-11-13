bbe190676121445c8cc7750891dd6ddcb31083a7
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-11-13 14:42:39 -0500

    cleaning code

--- rl_tools_controller.c
+++ rl_tools_controller.c
@@ -1,6 +1,5 @@
 #include "debug.h"
 #include "usec_time.h"
-// #include "policy_generated.h"
 #include <math.h>
 #include "math3d.h"
 #include "log.h"
@@ -17,28 +16,15 @@
 #include "pm.h"
 #include "task.h"
 
-// #include "dynamics_encoder.h"
-
-// #define DEBUG_OUTPUT_INTERVAL 500
 #define CONTROL_INTERVAL_MS 2
 #define CONTROL_INTERVAL_US (CONTROL_INTERVAL_MS * 1000)
 #define CONTROL_PACKET_TIMEOUT_USEC (1000*400)
 #define BEHIND_SCHEDULE_MESSAGE_MIN_INTERVAL (1000000)
 #define CONTROL_INVOCATION_INTERVAL_ALPHA 0.95f
 #define DEBUG_MEASURE_FORWARD_TIME
-// #define MIN_RPM 10000
 #define MIN_RPM 0
 #define MAX_RPM 21702.1
-// #define WAYPOINT_NAVIGATION
-// static uint8_t waypoint_navigation = 0;
-float WAYPOINT_NAVIGATION_POINT_DURATION = 4;
-#define WAYPOINT_NAVIGATION_POINTS (5)
-
-// #define PRINT_RPY
-// #define PRINT_TWIST
-
-// test stuff
-
+#define WAYPOINT_NAVIGATION_NUMBER_OF_POINTS (5)
 typedef enum ControllerState{
   STATE_RESET,
   STATE_FORWARD,
@@ -57,6 +43,7 @@ static uint64_t timestamp_last_reset;
 static uint64_t timestamp_last_behind_schedule_message = 0;
 static uint64_t timestamp_last_control_invocation = 0;
 static uint64_t timestamp_last_control_packet_received = 0;
+static uint64_t timestamp_controller_activation;
 
 // Logging variables
 static float control_invocation_interval = 0;
@@ -68,15 +55,16 @@ static float pos_error[3] = {0, 0, 0};
 static float relative_pos[3] = {0, 0, 0};
 static float origin[3] = {0, 0, 0};
 
-static float POS_DISTANCE_LIMIT_POSITION;
-static float VEL_DISTANCE_LIMIT_POSITION;
-static float POS_DISTANCE_LIMIT_FIGURE_EIGHT;
-static float VEL_DISTANCE_LIMIT_FIGURE_EIGHT;
-static float POS_DISTANCE_LIMIT_MELLINGER;
-static float VEL_DISTANCE_LIMIT_MELLINGER;
-static float POS_DISTANCE_LIMIT_BRESCIANI;
-static float VEL_DISTANCE_LIMIT_BRESCIANI;
-static uint8_t MELLINGER_ENABLE_INTEGRATORS;
+static float pos_distance_limit_position;
+static float vel_distance_limit_position;
+static float pos_distance_limit_figure_eight;
+static float vel_distance_limit_figure_eight;
+static float pos_distance_limit_mellinger;
+static float vel_distance_limit_mellinger;
+static float pos_distance_limit_bresciani;
+static float vel_distance_limit_bresciani;
+static uint8_t mellinger_enable_integrators;
+static uint8_t log_set_motors = 0;
 
 enum Mode{
   POSITION = 0,
@@ -85,34 +73,31 @@ enum Mode{
   FIGURE_EIGHT = 3
 };
 static uint8_t mode;
-static float trajectory[WAYPOINT_NAVIGATION_POINTS][3] = {
+static float trajectory[WAYPOINT_NAVIGATION_NUMBER_OF_POINTS][3] = {
   {0.0, 0.0, 0.0},
   {1.0, 0.0, 0.0},
   {1.0, 1.0, 0.0},
   {0.0, 1.0, 0.0},
   {0.0, 0.0, 0.0},
 };
-static uint8_t waypoint_navigation_dynamic_current_waypoint = 0;
-static float waypoint_navigation_dynamic_threshold = 0.1;
-static float figure_eight_interval = 5.5;
-static float figure_eight_warmup_time; 
-static float figure_eight_scale = 1.0;
-static float trajectory_scale = 0.5;
-static float figure_eight_progress = 0;
-static uint64_t figure_eight_last_invocation;
-static float target_height, target_height_figure_eight;
-static uint64_t timestamp_last_waypoint, timestamp_controller_activation;
-static uint8_t log_set_motors = 0;
+static float target_height;
 
-// NN input
+static uint64_t waypoint_navigation_timestamp_start;
+static uint8_t  waypoint_navigation_dynamic_current_waypoint = 0;
+static float    waypoint_navigation_dynamic_threshold = 0.1;
+static float    waypoint_navigation_point_duration = 4;
+static float    waypoint_navigation_trajectory_scale = 0.5;
+
+static float    figure_eight_interval = 5.5;
+static float    figure_eight_warmup_time; 
+static float    figure_eight_scale = 1.0;
+static float    figure_eight_progress = 0;
+static uint64_t figure_eight_last_invocation;
+static float    target_height_figure_eight;
+
 static float state_input[13];
 static float action_output[4];
-// static float position[3];
-// static float attitude_quat[4];
-// static float twist_linear[3];
-// static float twist_angular[3];
 
-// Control variables: output
 const uint8_t motors[4] = {MOTOR_M1, MOTOR_M2, MOTOR_M3, MOTOR_M4};
 static uint8_t set_motors_overwrite = 0;
 static uint16_t motor_cmd[4];
@@ -127,7 +112,6 @@ static setpoint_t last_setpoint;
 
 static uint8_t hand_test = 0; // 0 = off; 1 = setpoint; 2 = angular velocity rejection; 3 = angular velocity rejection + orientation rejection;
 static uint8_t use_orig_controller = 0;
-
 
 
 static inline float clip(float v, float low, float high){
@@ -146,7 +130,7 @@ static inline float clip(float v, float low, float high){
 
 static inline void update_state(const sensorData_t* sensors, const state_t* state){
   if(hand_test == 0){
-    float POS_DISTANCE_LIMIT = mode == FIGURE_EIGHT ? POS_DISTANCE_LIMIT_FIGURE_EIGHT : POS_DISTANCE_LIMIT_POSITION;
+    float POS_DISTANCE_LIMIT = mode == FIGURE_EIGHT ? pos_distance_limit_figure_eight : pos_distance_limit_position;
     state_input[ 0] = clip(state->position.x - target_pos[0], -POS_DISTANCE_LIMIT, POS_DISTANCE_LIMIT);
     state_input[ 1] = clip(state->position.y - target_pos[1], -POS_DISTANCE_LIMIT, POS_DISTANCE_LIMIT);
     state_input[ 2] = clip(state->position.z - target_pos[2], -POS_DISTANCE_LIMIT, POS_DISTANCE_LIMIT);
@@ -169,7 +153,7 @@ static inline void update_state(const sensorData_t* sensors, const state_t* stat
     state_input[ 6] = 0;
   }
   if(hand_test == 0){
-    float VEL_DISTANCE_LIMIT = mode == FIGURE_EIGHT ? VEL_DISTANCE_LIMIT_FIGURE_EIGHT : VEL_DISTANCE_LIMIT_POSITION;
+    float VEL_DISTANCE_LIMIT = mode == FIGURE_EIGHT ? vel_distance_limit_figure_eight : vel_distance_limit_position;
     state_input[ 7] = clip(state->velocity.x - target_vel[0], -VEL_DISTANCE_LIMIT, VEL_DISTANCE_LIMIT);
     state_input[ 8] = clip(state->velocity.y - target_vel[1], -VEL_DISTANCE_LIMIT, VEL_DISTANCE_LIMIT);
     state_input[ 9] = clip(state->velocity.z - target_vel[2], -VEL_DISTANCE_LIMIT, VEL_DISTANCE_LIMIT);
@@ -212,22 +196,22 @@ void controllerOutOfTreeInit(void){
   control_invocation_interval = 0;
   forward_tick = 0;
   hand_test = 0;
-  timestamp_last_waypoint = 0;
-  trajectory_scale = 0.5;
+  waypoint_navigation_timestamp_start = 0;
+  waypoint_navigation_trajectory_scale = 0.5;
   relative_pos[0] = 0;
   relative_pos[1] = 0;
   relative_pos[2] = 0;
   log_set_motors = 0;
 
-  POS_DISTANCE_LIMIT_POSITION = 0.5f;
-  VEL_DISTANCE_LIMIT_POSITION = 2.0f;
-  POS_DISTANCE_LIMIT_FIGURE_EIGHT = 0.6f;
-  VEL_DISTANCE_LIMIT_FIGURE_EIGHT = 2.0f;
-  POS_DISTANCE_LIMIT_MELLINGER = 0.2f;
-  VEL_DISTANCE_LIMIT_MELLINGER = 1.0f;
-  POS_DISTANCE_LIMIT_BRESCIANI = 0.2f;
-  VEL_DISTANCE_LIMIT_BRESCIANI = 1.0f;
-  MELLINGER_ENABLE_INTEGRATORS = 1;
+  pos_distance_limit_position = 0.5f;
+  vel_distance_limit_position = 2.0f;
+  pos_distance_limit_figure_eight = 0.6f;
+  vel_distance_limit_figure_eight = 2.0f;
+  pos_distance_limit_mellinger = 0.2f;
+  vel_distance_limit_mellinger = 1.0f;
+  pos_distance_limit_bresciani = 0.2f;
+  vel_distance_limit_bresciani = 1.0f;
+  mellinger_enable_integrators = 1;
 
   target_height = 0.0;
   target_height_figure_eight = 0.0;
@@ -335,9 +319,9 @@ void controllerOutOfTree(control_t *control, setpoint_t *setpoint, const sensorD
   timestamp_last_control_invocation = now;
   bool set_motors = (now - timestamp_last_control_packet_received < CONTROL_PACKET_TIMEOUT_USEC)  || (set_motors_overwrite == 1 && motor_cmd_divider >= 3);
   log_set_motors = set_motors ? 1 : 0;
-  set_rl_tools_overwrite_stabilizer(set_motors);
+  // set_rl_tools_overwrite_stabilizer(set_motors);
   if(!prev_set_motors && set_motors){
-    timestamp_last_waypoint = now;
+    waypoint_navigation_timestamp_start = now;
     timestamp_controller_activation = now;
     waypoint_navigation_dynamic_current_waypoint = 0;
     origin[0] = state->position.x;
@@ -347,7 +331,7 @@ void controllerOutOfTree(control_t *control, setpoint_t *setpoint, const sensorD
     figure_eight_progress = 0;
     controllerMellingerFirmwareInit();
     controllerINDIInit();
-    controllerMellingerFirmwareEnableIntegrators(MELLINGER_ENABLE_INTEGRATORS == 1);
+    // controllerMellingerFirmwareEnableIntegrators(MELLINGER_ENABLE_INTEGRATORS == 1);
     DEBUG_PRINT("Controller activated\n");
     switch(mode){
       case POSITION:
@@ -384,11 +368,11 @@ void controllerOutOfTree(control_t *control, setpoint_t *setpoint, const sensorD
       break;
     case WAYPOINT_NAVIGATION:
     {
-      uint64_t elapsed_since_start = (now-timestamp_last_waypoint);
-      int current_point = (elapsed_since_start / ((int)(WAYPOINT_NAVIGATION_POINT_DURATION * 1000 * 1000))) % WAYPOINT_NAVIGATION_POINTS;
-      target_pos[0] = trajectory[current_point][0] * trajectory_scale + origin[0];
-      target_pos[1] = trajectory[current_point][1] * trajectory_scale + origin[1];
-      target_pos[2] = trajectory[current_point][2] * trajectory_scale + origin[2];
+      uint64_t elapsed_since_start = (now-waypoint_navigation_timestamp_start);
+      int current_point = (elapsed_since_start / ((int)(waypoint_navigation_point_duration * 1000 * 1000))) % WAYPOINT_NAVIGATION_NUMBER_OF_POINTS;
+      target_pos[0] = trajectory[current_point][0] * waypoint_navigation_trajectory_scale + origin[0];
+      target_pos[1] = trajectory[current_point][1] * waypoint_navigation_trajectory_scale + origin[1];
+      target_pos[2] = trajectory[current_point][2] * waypoint_navigation_trajectory_scale + origin[2];
     }
       break;
     case WAYPOINT_NAVIGATION_DYNAMIC:
@@ -399,7 +383,7 @@ void controllerOutOfTree(control_t *control, setpoint_t *setpoint, const sensorD
 
         float current_dist = sqrtf(x*x + y*y + z*z);
         if(current_dist < waypoint_navigation_dynamic_threshold){
-          waypoint_navigation_dynamic_current_waypoint = (waypoint_navigation_dynamic_current_waypoint + 1) % WAYPOINT_NAVIGATION_POINTS;
+          waypoint_navigation_dynamic_current_waypoint = (waypoint_navigation_dynamic_current_waypoint + 1) % WAYPOINT_NAVIGATION_NUMBER_OF_POINTS;
           DEBUG_PRINT("Next waypoint %d, [%f, %f, %f]\n", waypoint_navigation_dynamic_current_waypoint, trajectory[waypoint_navigation_dynamic_current_waypoint][0], trajectory[waypoint_navigation_dynamic_current_waypoint][1], trajectory[waypoint_navigation_dynamic_current_waypoint][2]);
         }
         target_pos[0] = origin[0] + trajectory[waypoint_navigation_dynamic_current_waypoint][0];
@@ -514,12 +498,12 @@ void controllerOutOfTree(control_t *control, setpoint_t *setpoint, const sensorD
       }
       else{
         if(use_orig_controller == 2){
-          setpoint->position.x = state->position.x + clip(target_pos[0] - state->position.x, -POS_DISTANCE_LIMIT_MELLINGER, POS_DISTANCE_LIMIT_MELLINGER);
-          setpoint->position.y = state->position.y + clip(target_pos[1] - state->position.y, -POS_DISTANCE_LIMIT_MELLINGER, POS_DISTANCE_LIMIT_MELLINGER);
-          setpoint->position.z = state->position.z + clip(target_pos[2] - state->position.z, -POS_DISTANCE_LIMIT_MELLINGER, POS_DISTANCE_LIMIT_MELLINGER);
-          setpoint->velocity.x = state->velocity.x + clip(target_vel[0] - state->velocity.x, -VEL_DISTANCE_LIMIT_MELLINGER, VEL_DISTANCE_LIMIT_MELLINGER);
-          setpoint->velocity.y = state->velocity.y + clip(target_vel[1] - state->velocity.y, -VEL_DISTANCE_LIMIT_MELLINGER, VEL_DISTANCE_LIMIT_MELLINGER);
-          setpoint->velocity.z = state->velocity.z + clip(target_vel[2] - state->velocity.z, -VEL_DISTANCE_LIMIT_MELLINGER, VEL_DISTANCE_LIMIT_MELLINGER);
+          setpoint->position.x = state->position.x + clip(target_pos[0] - state->position.x, -pos_distance_limit_mellinger, pos_distance_limit_mellinger);
+          setpoint->position.y = state->position.y + clip(target_pos[1] - state->position.y, -pos_distance_limit_mellinger, pos_distance_limit_mellinger);
+          setpoint->position.z = state->position.z + clip(target_pos[2] - state->position.z, -pos_distance_limit_mellinger, pos_distance_limit_mellinger);
+          setpoint->velocity.x = state->velocity.x + clip(target_vel[0] - state->velocity.x, -vel_distance_limit_mellinger, vel_distance_limit_mellinger);
+          setpoint->velocity.y = state->velocity.y + clip(target_vel[1] - state->velocity.y, -vel_distance_limit_mellinger, vel_distance_limit_mellinger);
+          setpoint->velocity.z = state->velocity.z + clip(target_vel[2] - state->velocity.z, -vel_distance_limit_mellinger, vel_distance_limit_mellinger);
           controllerMellingerFirmware(control, setpoint, sensors, state, tick);
         }
         else{
@@ -527,12 +511,12 @@ void controllerOutOfTree(control_t *control, setpoint_t *setpoint, const sensorD
             controllerINDI(control, setpoint, sensors, state, tick);
           }
           else{
-            setpoint->position.x = state->position.x + clip(target_pos[0] - state->position.x, -POS_DISTANCE_LIMIT_BRESCIANI, POS_DISTANCE_LIMIT_BRESCIANI);
-            setpoint->position.y = state->position.y + clip(target_pos[1] - state->position.y, -POS_DISTANCE_LIMIT_BRESCIANI, POS_DISTANCE_LIMIT_BRESCIANI);
-            setpoint->position.z = state->position.z + clip(target_pos[2] - state->position.z, -POS_DISTANCE_LIMIT_BRESCIANI, POS_DISTANCE_LIMIT_BRESCIANI);
-            setpoint->velocity.x = state->velocity.x + clip(target_vel[0] - state->velocity.x, -VEL_DISTANCE_LIMIT_BRESCIANI, VEL_DISTANCE_LIMIT_BRESCIANI);
-            setpoint->velocity.y = state->velocity.y + clip(target_vel[1] - state->velocity.y, -VEL_DISTANCE_LIMIT_BRESCIANI, VEL_DISTANCE_LIMIT_BRESCIANI);
-            setpoint->velocity.z = state->velocity.z + clip(target_vel[2] - state->velocity.z, -VEL_DISTANCE_LIMIT_BRESCIANI, VEL_DISTANCE_LIMIT_BRESCIANI);
+            setpoint->position.x = state->position.x + clip(target_pos[0] - state->position.x, -pos_distance_limit_bresciani, pos_distance_limit_bresciani);
+            setpoint->position.y = state->position.y + clip(target_pos[1] - state->position.y, -pos_distance_limit_bresciani, pos_distance_limit_bresciani);
+            setpoint->position.z = state->position.z + clip(target_pos[2] - state->position.z, -pos_distance_limit_bresciani, pos_distance_limit_bresciani);
+            setpoint->velocity.x = state->velocity.x + clip(target_vel[0] - state->velocity.x, -vel_distance_limit_bresciani, vel_distance_limit_bresciani);
+            setpoint->velocity.y = state->velocity.y + clip(target_vel[1] - state->velocity.y, -vel_distance_limit_bresciani, vel_distance_limit_bresciani);
+            setpoint->velocity.z = state->velocity.z + clip(target_vel[2] - state->velocity.z, -vel_distance_limit_bresciani, vel_distance_limit_bresciani);
             controllerBrescianini(control, setpoint, sensors, state, tick);
           }
         }
@@ -555,53 +539,21 @@ PARAM_ADD(PARAM_FLOAT, target_z_fe, &target_height_figure_eight)
 PARAM_ADD(PARAM_UINT8, smo, &set_motors_overwrite)
 PARAM_ADD(PARAM_UINT8, ht, &hand_test)
 PARAM_ADD(PARAM_UINT8, wn, &mode)
-PARAM_ADD(PARAM_FLOAT, ts, &trajectory_scale)
+PARAM_ADD(PARAM_FLOAT, ts, &waypoint_navigation_trajectory_scale)
 PARAM_ADD(PARAM_FLOAT, wpt, &waypoint_navigation_dynamic_threshold)
+PARAM_ADD(PARAM_FLOAT, wni, &waypoint_navigation_point_duration)
 PARAM_ADD(PARAM_FLOAT, fewt, &figure_eight_warmup_time)
-PARAM_ADD(PARAM_FLOAT, fei, &figure_eight_interval)
-PARAM_ADD(PARAM_FLOAT, fes, &figure_eight_scale)
-PARAM_ADD(PARAM_FLOAT, pdlp, &POS_DISTANCE_LIMIT_POSITION)
-PARAM_ADD(PARAM_FLOAT, pdlfe, &POS_DISTANCE_LIMIT_FIGURE_EIGHT)
-PARAM_ADD(PARAM_FLOAT, vdlp, &VEL_DISTANCE_LIMIT_POSITION)
-PARAM_ADD(PARAM_FLOAT, vdlfe, &VEL_DISTANCE_LIMIT_FIGURE_EIGHT)
-PARAM_ADD(PARAM_FLOAT, pdlm, &POS_DISTANCE_LIMIT_MELLINGER)
-PARAM_ADD(PARAM_FLOAT, vdlm, &VEL_DISTANCE_LIMIT_MELLINGER)
+PARAM_ADD(PARAM_FLOAT, fei,  &figure_eight_interval)
+PARAM_ADD(PARAM_FLOAT, fes,  &figure_eight_scale)
+PARAM_ADD(PARAM_FLOAT, pdlp,  &pos_distance_limit_position)
+PARAM_ADD(PARAM_FLOAT, pdlfe, &pos_distance_limit_figure_eight)
+PARAM_ADD(PARAM_FLOAT, vdlp,  &vel_distance_limit_position)
+PARAM_ADD(PARAM_FLOAT, vdlfe, &vel_distance_limit_figure_eight)
+PARAM_ADD(PARAM_FLOAT, pdlm,  &pos_distance_limit_mellinger)
+PARAM_ADD(PARAM_FLOAT, vdlm,  &vel_distance_limit_mellinger)
 PARAM_ADD(PARAM_UINT8, orig, &use_orig_controller)
-PARAM_ADD(PARAM_UINT8, mei, &MELLINGER_ENABLE_INTEGRATORS)
-PARAM_ADD(PARAM_FLOAT, wni, &WAYPOINT_NAVIGATION_POINT_DURATION)
+PARAM_ADD(PARAM_UINT8, mei, &mellinger_enable_integrators)
 PARAM_GROUP_STOP(rlt)
-
-
-// LOG_GROUP_START(rltp)
-// LOG_ADD(LOG_FLOAT, x, &state_input[0])
-// LOG_ADD(LOG_FLOAT, y, &state_input[1])
-// LOG_ADD(LOG_FLOAT, z, &state_input[2])
-// LOG_GROUP_STOP(rltp)
-
-// LOG_GROUP_START(rltq)
-// LOG_ADD(LOG_FLOAT, w, &state_input[3])
-// LOG_ADD(LOG_FLOAT, x, &state_input[4])
-// LOG_ADD(LOG_FLOAT, y, &state_input[5])
-// LOG_ADD(LOG_FLOAT, z, &state_input[6])
-// LOG_GROUP_STOP(rltq)
-
-// LOG_GROUP_START(rlttwl)
-// LOG_ADD(LOG_FLOAT, x, &state_input[7])
-// LOG_ADD(LOG_FLOAT, y, &state_input[8])
-// LOG_ADD(LOG_FLOAT, z, &state_input[9])
-// LOG_GROUP_STOP(rlttwl)
-
-// LOG_GROUP_START(rlttwa)
-// LOG_ADD(LOG_FLOAT, x, &state_input[10])
-// LOG_ADD(LOG_FLOAT, y, &state_input[11])
-// LOG_ADD(LOG_FLOAT, z, &state_input[12])
-// LOG_GROUP_STOP(rlttwa)
-
-// LOG_GROUP_START(rltt)
-// LOG_ADD(LOG_FLOAT, x, &target_pos[0])
-// LOG_ADD(LOG_FLOAT, y, &target_pos[1])
-// LOG_ADD(LOG_FLOAT, z, &target_pos[2])
-// LOG_GROUP_STOP(rltt)
 
 
 LOG_GROUP_START(rltm)
